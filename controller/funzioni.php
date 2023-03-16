bcf2ad41f7071778c2b8583c5a0d6cd74ff65134
Doc-Inf <120362529+Doc-Inf@users.noreply.github.com>
2023-03-17 00:20:09 +0100

    Aggiunta Stampa Valori ultima giornata

--- controller/funzioni.php
+++ controller/funzioni.php
@@ -1,7 +1,8 @@
 <?php
     define("DEBUG_MODE",false);
     define("CONFIG_FILE","database/config.txt");
-    //define("DEBUG_MODE",true);  
+    define("KEY","vallauri");
+   
     println("Caricamento funzioni php completato<BR>");
 
     if (!file_exists(CONFIG_FILE)) {
@@ -9,9 +10,10 @@
     }
     $configData = explode("\n", file_get_contents("database/config.txt") );
         
-    $username = decrypt($configData[0],"vallauri");
-    $password = decrypt($configData[1],"vallauri");
-    $connection = getConnection('docente','vallauri','meteovelletri');
+    $username = decrypt($configData[0],KEY);
+    $password = decrypt($configData[1],KEY);
+    $database = decrypt($configData[2],KEY);
+    $connection = getConnection($username,$password,$database);
 
     function getConnection($username,$password,$database) : PDO{
         $connection;
@@ -40,6 +42,12 @@
         return $result;
     }
 
+    function getLast($connection){
+        $stmt = $connection->prepare("SELECT * FROM rilevazioni2023 r WHERE date(r.data)=(SELECT max(date(r2.data)) FROM rilevazioni2023 r2)");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
     function printData($data){
         echo "<TABLE>";
         foreach($data as $row){
@@ -50,6 +58,66 @@
             echo "</TR>";
         }
         echo "</TABLE>";
+    }
+
+    function printRow($row){
+        $infoData = explode(" ", $row["data"]); 
+        $dataRilevazione = $infoData[0];
+        $oraRilevazione = $infoData[1];
+        $pressione = $row["pressione"];
+        $umidita = $row["umidita"];
+        $direzioneVento = $row["direzioneVento"];
+        $intensitaVento = $row["ventoKm_h"];
+        $imgTemperatura;
+
+        $output = "";
+        $temp = $row["temperatura"];
+       
+        if( $temp < 10){
+            $imgTemperatura = "images/temperatura-freddo.png";
+        }else{
+            if($temp >= 10 && $temp <20){
+                $imgTemperatura = "images/temperaturaAutunno.jpg";
+            }else{
+                if($temp >= 20 && $temp <35){
+                    $imgTemperatura = "images/caldo-estivo.jpg"; 
+                }else{
+                    $imgTemperatura = "images/caldissimo.webp";     
+                }                
+            }
+        }
+        $output = <<<END
+                        <div class="container">
+                            <div class="row mt-5 mb-5 justify-content-between">
+                                <div class="col-sm-12 col-md-5 col-lg-3  text-center shadow-lg rounded mb-5 me-1 pt-3 pd-3 pe-3">
+                                    <img class="img-fluid mb-3 rounded shadow-lg" src="images/calendario2.webp" alt="Calendario">
+                                    <p><strong>Data: </strong> $dataRilevazione <strong class="ms-4">Ora: </strong> $oraRilevazione</p>                                    
+                                </div>
+                                <div class="col-sm-12 col-md-5 col-lg-3 text-center shadow-lg rounded mb-5 me-1 pt-3 pd-3 pe-3">
+                                    <img class="img-fluid mb-3 rounded shadow-lg" src="$imgTemperatura" alt="Temperatura">
+                                    <p><strong>Temperatura:</strong> $temp °C</p>
+                                </div>
+                                <div class="col-sm-12 col-md-5 col-lg-3  text-center shadow-lg rounded mb-5 me-1 pt-3 pd-3 pe-3">
+                                    <img class="img-fluid mb-3 rounded shadow-lg" src="images/barometro2.webp" alt="Barometro">
+                                    <p><strong>Pressione: </strong> $pressione </p>
+                                </div>
+                                <div class="col-sm-12 col-md-5 col-lg-3  text-center shadow-lg rounded mb-5 me-1 pt-3 pd-3 pe-3">
+                                    <img class="img-fluid mb-3 rounded shadow-lg" src="images/umidita2.webp" alt="Umidità">
+                                    <p><strong>Umidità: </strong> $umidita </p>                                
+                                </div>
+                                <div class="col-sm-12 col-md-5 col-lg-3  text-center shadow-lg rounded mb-5 me-1 pt-3 pd-3 pe-3">
+                                    <img class="img-fluid mb-3 rounded shadow-lg" src="images/paleVento.jpg" alt="Direzione vento">
+                                    <p><strong>Direzione Vento: </strong> $direzioneVento </p>
+                                </div>
+                                <div class="col-sm-12 col-md-5 col-lg-3  text-center shadow-lg rounded mb-5 me-1 pt-3 pd-3 pe-3">
+                                    <img class="img-fluid mb-3 rounded shadow-lg" src="images/forzaVento5.jpg" alt="Forza Vento">
+                                    <p><strong>Intensità vento: </strong> $intensitaVento </p>
+                                </div>
+                            </div>
+                        </div>
+                        END;
+        echo $output;
+        
     }
 
     function println($message){
--- index.php
+++ index.php
@@ -54,10 +54,11 @@
           <?php 
             require_once "controller/funzioni.php";
             
-            
+            printRow(getLast($connection)[0]);
+            /*
             $sql = "SELECT * FROM rilevazioni2023";
             $result = query($connection,$sql);
-            printData($result);
+            printData($result);*/
           ?>
     </div>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
